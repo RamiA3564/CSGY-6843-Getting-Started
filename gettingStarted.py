a9049ed8427f1c2be9b4a25ffd76b40d99b3a9c7
Rami Gorovoi-Abu Hashish <ra3564@nyu.edu>
2026-01-27 14:24:11 -0500

    Edit answer
    Misundertood where the error was

--- gettingStarted.py
+++ gettingStarted.py
@@ -10,7 +10,7 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-       answer  = str('pcap')
+       answer  = 'pcap'
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = 'Yes'
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
@@ -20,7 +20,7 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = 'No'
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = int(4)
+        answer = int(5)
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = int(3)
     else:
